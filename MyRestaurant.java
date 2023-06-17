44198aff8397d28f01ea5d2e8b12b2752b7d8aec
Dedios <dediosprogramming@gmail.com>
2023-06-17 19:03:27 -0400

    adding constructors

--- MyRestaurant.java
+++ MyRestaurant.java
@@ -12,6 +12,25 @@ public class MyRestaurant extends JFrame {
         Name = new JLabel("Name:");
         Contact = new JLabel("Contact");
 
+        tfNumber = new JTextField();
+        tfName = new JTextField();
+        tfContact = new JTextField();
+
+        Customer.setBounds();
+        tfNumber.setBounds();
+        Name.setBounds();
+        tfName.setBounds();
+        Contact.setBounds();
+        tfContact.setBounds();
+
+        getContentPane().add(Customer);
+        getContentPane().add(tfNumber);
+        getContentPane().add(Name);
+        getContentPane().add(tfName);
+        getContentPane().add(Contact);
+        getContentPane().add(tfContact);
+
+
 
 
 
