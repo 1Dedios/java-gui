cff99beddf55024632203494be18133c46544041
Dedios <dediosprogramming@gmail.com>
2023-06-17 21:34:24 -0400

    added more labels and a button

--- MyRestaurant.java
+++ MyRestaurant.java
@@ -1,36 +1,71 @@
 import javax.swing.*;
 
 public class MyRestaurant extends JFrame {
-    JLabel Customer, Name, Contact;
-    JTextField tfNumber, tfName, tfContact;
+    JLabel customer, name, contact, food, drink, type;
+    JTextField tfNumber, tfName, tfContact, tfFood, tfDrink, tfType;
+    JButton reset, print, receipt;
 
     public MyRestaurant() {
         setTitle("My Sophisticated Restaurant");
-        setSize(600, 500);
+        setSize(700, 500);
         setLayout(null);
 
-        Customer = new JLabel("Customer No:");
-        Name = new JLabel("Name:");
-        Contact = new JLabel("Contact:");
+        customer = new JLabel("Customer No:");
+        name = new JLabel("Name:");
+        contact = new JLabel("Contact:");
+        food = new JLabel("Foods:");
+        drink = new JLabel("Drink:");
+        type = new JLabel("Type:");
 
         tfNumber = new JTextField();
         tfName = new JTextField();
         tfContact = new JTextField();
+        tfDrink = new JTextField();
+        tfType = new JTextField();
 
-        Customer.setBounds(20, 50, 120, 30);
+        customer.setBounds(20, 50, 120, 30);
         tfNumber.setBounds(120, 50, 150, 30);
-        Name.setBounds(20, 80, 50, 30);
+
+        name.setBounds(20, 80, 50, 30);
         tfName.setBounds(120, 80 , 150, 30);
-        Contact.setBounds(20, 110, 100, 30);
+
+        contact.setBounds(20, 110, 100, 30);
         tfContact.setBounds(120, 110, 150, 30);
 
-        getContentPane().add(Customer);
-        getContentPane().add(tfNumber);
-        getContentPane().add(Name);
-        getContentPane().add(tfName);
-        getContentPane().add(Contact);
-        getContentPane().add(tfContact);
+        food.setBounds(20, 200, 120, 30);
+        //tfFood.setBounds(0, 140, 150, 30);
 
+        drink.setBounds(20, 250, 120, 30);
+        //tfDrink.setBounds(120, 170, );
+
+        type.setBounds(20, 300, 120, 30);
+        //tfType.setBounds(120, 200, 30);
+
+        reset = new JButton("Reset");
+        reset.setBounds(400, 100, 80, 40);
+
+
+        getContentPane().add(customer);
+        getContentPane().add(tfNumber);
+        getContentPane().add(name);
+        getContentPane().add(tfName);
+        getContentPane().add(contact);
+        getContentPane().add(tfContact);
+        getContentPane().add(food);
+        //getContentPane().add(tfFood);
+        getContentPane().add(drink);
+        getContentPane().add(tfDrink);
+        getContentPane().add(type);
+        getContentPane().add(tfType);
+        getContentPane().add(reset);
+
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
         setVisible(true);
         setVisible(true);
         setVisible(true);
