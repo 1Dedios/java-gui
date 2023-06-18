fdac331b82b22507080a48779fe13e36979718c8
Dedios <dediosprogramming@gmail.com>
2023-06-17 20:26:41 -0400

    setBound() method implementation

--- MyRestaurant.java
+++ MyRestaurant.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class MyRestaurant extends JFrame {
     JLabel Customer, Name, Contact;
     JTextField tfNumber, tfName, tfContact;
+
     public MyRestaurant() {
         setTitle("My Sophisticated Restaurant");
         setSize(600, 500);
@@ -16,12 +17,12 @@ public class MyRestaurant extends JFrame {
         tfName = new JTextField();
         tfContact = new JTextField();
 
-        Customer.setBounds();
-        tfNumber.setBounds();
-        Name.setBounds();
-        tfName.setBounds();
-        Contact.setBounds();
-        tfContact.setBounds();
+        Customer.setBounds(20, 50, 120, 30);
+        tfNumber.setBounds(50, 50, 120, 30);
+        Name.setBounds(20, 50, 50, 50);
+        tfName.setBounds(20, 40 , 40, 50);
+        Contact.setBounds(20, 80, 50, 70);
+        tfContact.setBounds(20, 130, 90, 100);
 
         getContentPane().add(Customer);
         getContentPane().add(tfNumber);
