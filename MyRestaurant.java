1c29afae4988af0da2faef22701a4cf719ca093e
Dedios <dediosprogramming@gmail.com>
2025-04-25 22:36:17 -0400

    Refactored README & rm unneccessary files

---
+++ MyRestaurant.java
@@ -0,0 +1,184 @@
+//package com.newrestaurant;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+
+public class MyRestaurant extends JFrame {
+    JLabel customer;
+    JLabel name;
+    JLabel contact;
+    JLabel food;
+    JLabel drink;
+    JLabel type;
+
+    JTextField tfNumber;
+    JTextField tfName;
+    JTextField tfContact;
+
+    JButton reset;
+    JButton print;
+    JButton receipt;
+
+    JComboBox cb1;
+    JComboBox cb2;
+
+
+    JRadioButton r1;
+    JRadioButton r2;
+
+    JTextArea area1;
+
+    public MyRestaurant() {
+        setTitle("My Sophisticated Restaurant");
+        setSize(700, 650);
+        setLayout(null);
+
+        customer = new JLabel("Customer No:");
+        name = new JLabel("Name:");
+        contact = new JLabel("Contact:");
+        food = new JLabel("Foods:");
+        drink = new JLabel("Drink:");
+        type = new JLabel("Type:");
+
+        tfNumber = new JTextField();
+        tfName = new JTextField();
+        tfContact = new JTextField();
+
+
+        reset = new JButton("Reset");
+        print = new JButton("Print");
+        receipt = new JButton("Receipt");
+
+
+        customer.setBounds(20, 50, 120, 30);
+        tfNumber.setBounds(120, 50, 150, 30);
+
+        name.setBounds(20, 80, 50, 30);
+        tfName.setBounds(120, 80 , 150, 30);
+
+        contact.setBounds(20, 110, 100, 30);
+        tfContact.setBounds(120, 110, 150, 30);
+
+        food.setBounds(20, 200, 120, 30);
+        String[] foods = {"Pepperoni Pizza", "Salmon Roll", "Cheese Burger", "Penne a la Vodka" };
+        cb1 = new JComboBox(foods);
+        cb1.setBounds(100, 200, 160,30);
+
+
+        drink.setBounds(20, 250, 120, 30);
+        String[] drinks = {"Lemonade", "Raspberry Iced Tea", "Pink Lemonade", "Signature House Drink"};
+        cb2 = new JComboBox(drinks);
+        cb2.setBounds(100, 250, 160, 30);
+
+
+        type.setBounds(20, 300, 120, 30);
+        r1 = new JRadioButton("Diet");
+        r2 = new JRadioButton("Regular");
+        r1.setBounds(100, 300, 60, 30);
+        r2.setBounds(170, 300,100, 30);
+        // ButtonGroup class to avoid two radio button selections
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+
+
+
+        reset.setBounds(395, 100, 80, 40);
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfNumber.setText("");
+                tfContact.setText("");
+                tfName.setText("");
+                area1.setText("");
+            }
+        });
+
+        receipt.setBounds(470, 100, 80, 40);
+        receipt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                area1.setText("My Sophisticated Restaurant\n");
+                area1.setText(area1.getText() + "\n" + "Customer No: " +
+                        tfNumber.getText() + "\n" + "Name: " +
+                        tfName.getText() + "\n" + "Contact: " +
+                        tfContact.getText());
+                area1.setText(area1.getText() + "\n" + "\nFood: " + cb1.getSelectedItem() + "\nDrink: " + cb2.getSelectedItem());
+            }
+        });
+
+        print.setBounds(545, 100, 80, 40);
+        print.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    area1.print();
+                } catch (PrinterException example) {
+                    System.out.println(example.getMessage());
+                }
+            }
+        });
+
+        area1 = new JTextArea();
+        area1.setBounds(395, 150, 240, 300);
+
+
+        getContentPane().add(customer);
+        getContentPane().add(tfNumber);
+
+        getContentPane().add(name);
+        getContentPane().add(tfName);
+
+        getContentPane().add(contact);
+        getContentPane().add(tfContact);
+
+        getContentPane().add(food);
+        getContentPane().add(cb1);
+
+        getContentPane().add(drink);
+        getContentPane().add(cb2);
+
+        getContentPane().add(type);
+        getContentPane().add(r1);
+        getContentPane().add(r2);
+
+
+        getContentPane().add(reset);
+        getContentPane().add(print);
+        getContentPane().add(receipt);
+        getContentPane().add(area1);
+
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+        setVisible(true);
+
+
+
+
+
+
+
+    }
+    public static void main (String [] args) {
+        new MyRestaurant();
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# Restaurant Interface
-A restaurant app in Java that has a graphical user interface (GUI). This allows users to interact with the app. 
+# Restaurant App
 
-### Components
-Created through the use of the Swing package in Java 11. 
+A restaurant order taker and receipt generator in Java. Built with the Swing GUI package in Java 11.
+
+## Demo
+
+<video width="320" height="250" controls>
+  <source src="media/app-video.mov" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+
+## Screenshots
+
+### Selection View
+
+<image src="media/screenshot.png" width="700px" height="700px">
+
+### Receipt Generation
+
+<image src="media/screenshot1.png" width="700px" height="700px">
